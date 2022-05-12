16a82247a35c988f6d907eefa189e35d5e89bfb7
Nasir <nasir.haidari@hotmail.com>
2022-05-12 11:43:24 +0200

    changes in ap1

--- server.js
+++ server.js
@@ -9,40 +9,33 @@ dotenv.config({ path: './config/config.env' })
 //Route files
 
 const app = express()
-
-//Mount routers
 app.use(cors())
 
-app.get('/test', (req, res) => {
-  res.send('Hello!')
+app.use('/api/v1/', (req, res) => {
+  res.send('Hello!!!!!!!!')
 })
 
 app.get('/api/v1/my-gallery', async (req, res) => {
   let url = `https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=${process.env.API_KEY}&gallery_id=72157720607928857&get_gallery_info=&format=json&nojsoncallback=1`
-  if (req.method === 'GET') {
-    try {
-      const { data } = await axios.get(url)
-      const list = data.photos.photo.map((photo) => {
-        return {
-          id: photo.id,
-          title: photo.title,
-          url: `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`,
-        }
-      })
 
-      res.status(200).json({
-        status: 'success',
-        list,
-      })
-    } catch (err) {
-      res.status(500).json({
-        status: 500,
-        message: err.message,
-      })
-    }
-  } else {
-    res.status(405).send({
-      error: 'Method not allowed',
+  try {
+    const { data } = await axios.get(url)
+    const list = data.photos.photo.map((photo) => {
+      return {
+        id: photo.id,
+        title: photo.title,
+        url: `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`,
+      }
+    })
+
+    res.status(200).json({
+      status: 'success',
+      list,
+    })
+  } catch (err) {
+    res.status(500).json({
+      status: 500,
+      message: err.message,
     })
   }
 })
@@ -50,7 +43,5 @@ app.get('/api/v1/my-gallery', async (req, res) => {
 const PORT = process.env.PORT || 6000
 
 app.listen(PORT, () => {
-  console.log(
-    `Server running on port  ${PORT} , running on ${process.env.NODE_ENV} mode`
-  )
+  console.log(`Server running on port  ${PORT} `)
 })
