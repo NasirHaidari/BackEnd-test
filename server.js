7c96091d9e0eb84a7e47c0075edef739831e54c5
Nasir <nasir.haidari@hotmail.com>
2022-05-12 12:21:54 +0200

    added few new gallerys

--- server.js
+++ server.js
@@ -11,13 +11,19 @@ dotenv.config({ path: './config/config.env' })
 const app = express()
 app.use(cors())
 
-app.use('/api/v1/', (req, res) => {
-  res.send('Hello!!!!!!!!')
-})
-
 app.get('/api/v1/my-gallery', async (req, res) => {
-  let url = `https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=${process.env.API_KEY}&gallery_id=72157720607928857&get_gallery_info=&format=json&nojsoncallback=1`
-
+  const galleryIds = [
+    '72157720409510023',
+    '72157720607928857',
+    '72157720530279238',
+    '72157720529928239',
+    '72157720474604239',
+    '72157720411155991',
+    '72157720471687824',
+  ]
+  let id = galleryIds[Math.floor(Math.random() * galleryIds.length)]
+  //let url = `https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=${process.env.API_KEY}&gallery_id=72157720607928857&get_gallery_info=&format=json&nojsoncallback=1`
+  let url = `https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=${process.env.API_KEY}&gallery_id=${id}&get_gallery_info=&format=json&nojsoncallback=1`
   try {
     const { data } = await axios.get(url)
     const list = data.photos.photo.map((photo) => {
